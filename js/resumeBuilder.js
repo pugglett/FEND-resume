a4f8ad0dc7e831d47deea8fceb4af673a247cb55
Lee R <leer5501@gmail.com>
2016-09-26 16:09:12 -0500

    Add objects to resume builder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,22 +8,22 @@ This is empty on purpose! Your code to build the resume will go here.
 // after finished with this stage
 // JAVASCRIPT PRACTICE
 
-// var awesomeThoughts = 
+// var awesomeThoughts =
 // 	"I am Lee and I am AWESOME!";
 // console.log(awesomeThoughts);
 
-// var funThoughts = 
+// var funThoughts =
 // 	awesomeThoughts.replace("AWESOME", "FUN");
 // console.log(funThoughts);
 
 // $("#main").append(funThoughts);
 
-var name = 
+var name =
 	"Lee Roberts";
-var role = 
+var role =
 	"Teaching Assistant";
 
-var formattedName = 
+var formattedName =
 	HTMLheaderName.replace("%data%", name);
 
 var formattedRole =
@@ -31,3 +31,101 @@ var formattedRole =
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+// var skills =
+// 	["stuntin' on hoes", "teaching", "being gay"];
+
+// $("#main").append(skills[0]);
+
+var bio = {
+	"name" : "Lee Roberts",
+	"role" : "Teaching Assistant",
+	"contacts" : {
+		"mobile" : "773.386.1191",
+		"email" : "leer5501@gmail.com",
+		"github" : "pugglett",
+		"instagram" : "_pugg",
+		"location" : "Chicago"
+	},
+	"welcomeMsg" : "thanks for checking me out!",
+	"skills" : [
+		"stuntin' on hoes", "teaching", "being gay"
+	],
+	"bioPic" : "images/avatar.png"
+}
+
+var formattedWelcomeMsg =
+	HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+
+var formattedBioPic =
+	HTMLbioPic.replace("%data%", bio.bioPic);
+
+
+$("#header").append(formattedWelcomeMsg);
+$("#header").append(formattedBioPic);
+
+var work = {};
+
+work.position = "Academic Team Teaching Assistant";
+work.employer = "Year Up";
+work.years = "2014 - Present";
+work.city = "Chicago";
+
+var formattedWorkEmployer =
+	HTMLworkEmployer.replace("%data%", work.employer);
+
+var formattedWorkTitle =
+	HTMLworkTitle.replace("%data%", work.position);
+
+var formattedWorkDates =
+	HTMLworkDates.replace("%data%", work.years);
+
+$("#workExperience").append(HTMLworkStart);
+$("#workExperience").append(formattedWorkEmployer);
+$("#workExperience").append(formattedWorkTitle);
+$("#workExperience").append(formattedWorkDates);
+
+// var education = {};
+// education["name"] = "Robert Morris University";
+// education["years"] = "2010 - 2012";
+// education["city"] = "Chicago";
+
+// var formattedSchoolName =
+// 	HTMLschoolName.replace("%data%", education["name"]);
+
+// var formattedSchoolDates =
+// 	HTMLschoolDates.replace("%data%", education["years"]);
+
+// var formattedSchoolCity =
+// 	HTMLschoolLocation.replace("%data%", education["city"]);
+
+// $("#education").append(HTMLschoolStart);
+// $("#education").append(formattedSchoolName);
+// $("#education").append(formattedSchoolCity);
+// $("#education").append(formattedSchoolDates);
+
+// CREATING A JSON FOR EDUCATION INFO
+var education = {
+	"schools" : [
+		{
+			"name" : "Robert Morris University",
+			"city" : "Chicago",
+			"degree" : "AA",
+			"major" : "Graphic Design"
+		}
+	],
+	"onlineClasses" : [
+		{
+			"title" : "Front End Web Development Nanodegree",
+			"school" : "Udacity",
+			"dates" : "2015 - 2016",
+			"url" : "www.udacity.com"
+		},
+		{
+			"title" : "Intro to Programming Nanodegree",
+			"school" : "Udacity",
+			"dates" : "2016",
+			"url" : "www.udacity.com"
+		}
+	]
+}
---
+++ js/resumeBuilderNotes.js
@@ -0,0 +1,71 @@
+var bio = {
+	"name" : "Lee Roberts",
+	"role" : "Teaching Assistant",
+	"contacts" : {
+		"mobile" : "773.386.1191",
+		"email" : "leer5501@gmail.com",
+		"github" : "pugglett",
+		"location" : "Chicago"
+	},
+	"welcomeMessage" : "thanks for checking me out!",
+	"skills" : [
+		"stuntin' on hoes", "teaching", "being gay"
+	],
+	"biopic" : "images/avatar.png"
+}
+
+var projects = {
+	"projects" : [
+		{
+			"title" : "Front End Nanodegree Resume",
+			"dates" : "2016",
+			"description" : "The first project for a Udacity Nanodegree related to web development. Required the use of skills in HTML, CSS, frameworks, and GitHub.",
+			"images" : [""]
+		}
+	]
+}
+
+var work = {
+	"jobs" : [
+		{
+			"employer" : "Year Up",
+			"title" : "Academic Team Teaching Assistant",
+			"location" : "Chicago",
+			"dates" : "2014 - 2016",
+			"description" : "Teaching CIS302, COM150, and BUS101."
+		},
+		{
+			"employer" : "commonground Marketing",
+			"title" : "Digital Production Intern",
+			"location" : "Chicago",
+			"dates" : "2013 - 2014",
+			"description" : "Assisting with creating of digital content such as landing pages. Reviewed user-submitted content for such pages and conducted video interviews."
+		}
+	]
+}
+
+var education = {
+	"schools" : [
+		{
+			"name" : "Robert Morris University",
+			"location" : "Chicago",
+			"dates" : 2016,
+			"url" : "",
+			"majors" : ["Graphic Design"]
+		}
+	],
+	"onlineClasses" : [
+		{
+			"title" : "Front End Web Development Nanodegree",
+			"school" : "Udacity",
+			"dates" : 2016,
+			"url" : "www.udacity.com"
+		},
+		{
+			"title" : "Intro to Programming Nanodegree",
+			"school" : "Udacity",
+			"dates" : 2016,
+			"url" : "www.udacity.com"
+		}
+	]
+}
